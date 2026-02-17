5ec0ba2291b1fd33348dda9e7fd7d53a1ab0349e
Elias <eliasventuri@gmail.com>
2026-02-17 11:18:16 +0700

    Use generic task configuration for Kotlin JVM target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
