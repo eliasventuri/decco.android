c5c26d106365b2adb0bb87558f8df6fcec4319f4
Elias <eliasventuri@gmail.com>
2026-02-18 13:15:09 +0700

    Release v1.0.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.decco.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.0.22"
+        versionCode = 24
+        versionName = "1.0.23"
         multiDexEnabled = true
     }
 
