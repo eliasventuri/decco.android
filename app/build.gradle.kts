aafccc2e54eedfc0d3f82f38b682bc6569947d45
Elias <eliasventuri@gmail.com>
2026-02-18 12:46:30 +0700

    Release v1.0.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.decco.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.0.13"
+        versionCode = 15
+        versionName = "1.0.14"
         multiDexEnabled = true
     }
 
