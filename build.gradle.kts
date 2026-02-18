40c92b42e2f962498ab51ff9fd178a48057a5054
Elias <eliasventuri@gmail.com>
2026-02-18 11:16:42 +0700

    Update Google Services plugin to 4.4.2 for Crashlytics 3.0.3 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "9.0.1" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.3" apply false
 }
