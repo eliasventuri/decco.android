06a6fad024ac59f6d3d86f0342d89ea15f8b9564
Elias <eliasventuri@gmail.com>
2026-05-18 14:15:52 +0700

    Release v1.0.29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.decco.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 29
-        versionName = "1.0.28"
+        versionCode = 30
+        versionName = "1.0.29"
         multiDexEnabled = true
     }
 
