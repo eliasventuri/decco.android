da254da5ae2812bd32f82b6fcd55ebb0f39343cd
Elias <eliasventuri@gmail.com>
2026-06-08 14:29:35 +0700

    Release v1.0.51

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.decco.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 51
-        versionName = "1.0.50"
+        versionCode = 52
+        versionName = "1.0.51"
         multiDexEnabled = true
     }
 
